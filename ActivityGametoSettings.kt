6064ce0041adc70e494ec0c5e7c398f85d942097
ayachi01 <127912966+ayachi01@users.noreply.github.com>
2024-09-13 23:04:03 +0800

    Update ActivityGametoSettings.kt
    added sounds on buttons.

--- ActivityGametoSettings.kt
+++ ActivityGametoSettings.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.media.SoundPool
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -19,11 +20,17 @@ class ActivityGametoSettings : AppCompatActivity() {
         setContentView(R.layout.activity_gameto_settings)
 
         this.settingstohome = findViewById(R.id.settingstohome)
+        val soundPool = SoundPool.Builder().build()
+        val soundId = soundPool.load(this, R.raw.mouse_click, 1)
         settingstohome.setOnClickListener {
+            soundPool.play(soundId, 1f, 1f, 1, 0, 1f)
             val intent2 = Intent(this, ActivityGame::class.java)
             startActivity(intent2) }
+
+
         this.quit = findViewById(R.id.quit)
         quit.setOnClickListener {
+            soundPool.play(soundId, 1f, 1f, 1, 0, 1f)
             val intent3 = Intent(this, MainActivity::class.java)
             startActivity(intent3)
         }
